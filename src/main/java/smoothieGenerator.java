57594d4812ae4976d17ec0062a2a860fde1a70a9
Jenny Dosti <jenny.dosti@alfresco.com>
2018-02-06 22:50:22 +0000

    My first test

---
+++ src/main/java/smoothieGenerator.java
@@ -0,0 +1,11 @@
+/**
+ * Created by guest on 06/02/2018.
+ */
+
+public class smoothieGenerator {
+
+    public static void main(String[] args){
+
+        System.out.println("This is a smoothie");
+    }
+}
